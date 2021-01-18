ae28424b4cad0167e0579e9bfaf620bdfe1e3471
roots <mcwangyi163@163.com>
2021-01-18 19:14:23 +0800

    clone的工程添加的内容

--- test_git/src/main/java/com/myjava/test/test_gits.java
+++ test_git/src/main/java/com/myjava/test/test_gits.java
@@ -7,5 +7,8 @@ public class test_gits {
         System.out.println("2222222dev分支添加");       //合并分支之后
         System.out.println("3333333master");
         System.out.println("444444dev添加");
+        System.out.println("clone的工程添加的内容");
+        System.out.println("clone的工程添加的内容");
+        System.out.println("clone的工程添加的内容");
     }
 }
