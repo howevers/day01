9d4aa887963cbcfdc000f94562acd87e1ebbb28e
roots <mcwangyi163@163.com>
2021-01-18 18:43:42 +0800

    master添加333333

--- test_git/src/main/java/com/myjava/test/test_gits.java
+++ test_git/src/main/java/com/myjava/test/test_gits.java
@@ -4,6 +4,7 @@ public class test_gits {
 
     public static void main(String[] args) {
         System.out.println("1111111");
-        System.out.println("2222222dev分支添加");
+        System.out.println("2222222dev分支添加");       //合并分支之后
+        System.out.println("3333333master");
     }
 }
